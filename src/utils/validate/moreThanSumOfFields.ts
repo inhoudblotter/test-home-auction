c6285c4efa3c5fe8a70af6568d3419958222d37c
inhoudblotter <inhoudblotter@gmail.com>
2024-11-12 21:07:50 +0500

    fix: the upper border of moreThanSumOfFields has been moved

--- src/utils/validate/moreThanSumOfFields.ts
+++ src/utils/validate/moreThanSumOfFields.ts
@@ -8,7 +8,7 @@ addMethod(
     message: string = 'The value must be greater than in the fields {...}'
   ) {
     return this.when(fields, values => {
-      return this.moreThan(
+      return this.min(
         values.reduce((acc, v) => (v ? acc + Number(v) : acc), 0),
         message.replace('{...}', fields.join(', '))
       );
